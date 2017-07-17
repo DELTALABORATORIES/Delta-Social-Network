d77b013905bb92ae18993538d9e7cd638614d48b
DELTALABORATORIES <deathadderdelta@gmail.com>
2017-07-17 14:31:14 +0400

    Working SignUp and SignIn system, Applied classes

--- PHP/index.php
+++ PHP/index.php
@@ -6,26 +6,3 @@
  * Time: 2:40 PM
  */
 
-
-//Sign In Page Validation
-if (!preg_match("/^[a-zA-Z ]*$/",$_POST['logInUSERNAME']) && !preg_match("/^[a-zA-Z ]*$/",$_POST['logInPASSWORD'])) {
-    header("Location:../index.html");
-}
-elseif ($_POST['logInUSERNAME'] == null || $_POST['logInPASSWORD'] == null){
-    header("Location:../index.html");
-}
-
-
-//Sign In Page Worked
-if ($_POST['logInUSERNAME'] == 'Delta@gmail.com' && $_POST['logInPASSWORD'] == 'Delta'){
-    header("Location:../timeline_page.html");
-}
-
-
-//Sign Up Validation
-if (!preg_match("/^[a-zA-Z ]*$/",$_POST['logUpName']) || !preg_match("/^[a-zA-Z ]*$/",$_POST['logUpLastName']) || !preg_match("/^[a-zA-Z ]*$/",$_POST['logUpUsername']) || !preg_match("/^[a-zA-Z ]*$/",$_POST['logUpPassword'])) {
-    header("Location:../registration_form.html");
-}
-else{
-    //Create Profile
-}
